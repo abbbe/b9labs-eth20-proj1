d0be627247cf047d540c2f8d1bcc23b65c17d082
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-25 21:51:43 +0100

    redone with truffle-3.4.11

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
       network_id: "*"
     },
 
+    geth_ropsten: {
+      host: "127.0.0.1",
+      port: 38545,
+      network_id: "3",
+      from: "0xe2DBC1817A18d345d051a348ceF998f3c14C2033"
+    },
+
     geth_rinkeby: {
       host: "127.0.0.1",
       port: 8545,
