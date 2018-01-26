1c2c6da86b5bae75284cbe700f15867cf6d7be4b
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-26 23:47:47 +0100

    show accounts used for contract deployment

--- migrations/2_deploy_splitter_contract.js
+++ migrations/2_deploy_splitter_contract.js
@@ -1,5 +1,6 @@
 const Splitter = artifacts.require("./Splitter.sol")
 
 module.exports = function(deployer, network, accounts) {
+	console.log("Accounts:", accounts[1], accounts[2], accounts[3]);
 	deployer.deploy(Splitter, accounts[1], accounts[2], accounts[3]);
 };
